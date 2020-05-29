f4fdb91e44c4db56dd7e8506a14dd0b65f3c76ef
Jeff Lett <jeffctown@gmail.com>
2020-05-29 13:15:50 -0400

    Adding a README

--- LibraryOnly/generate_xcframework.sh
+++ LibraryOnly/generate_xcframework.sh
@@ -1,13 +1,24 @@
 #!/bin/zsh
 
+# print commands
+set -x
+
+# clean up
+rm -rf LibraryOnly.xcframework
+rm -rf archives
+
+# env vars
 PROJECT_NAME="LibraryOnly"
 SCHEME_NAME="LibraryOnly-Package"
 FRAMEWORK_NAME="LibraryOnly"
+
+# Steps to Create XCFramework when we tag a new Package release
 
 swift package generate-xcodeproj
 xcodebuild -project "$PROJECT_NAME.xcodeproj" -list
 
 set -o pipefail
+
 platforms=("iOS" "iOS Simulator" "watchOS" "watchOS Simulator" "tvOS" "tvOS Simulator" "macOS")
 for platform in "${platforms[@]}"; do
         xcodebuild archive -project "$PROJECT_NAME.xcodeproj" -scheme "$SCHEME_NAME" \
@@ -15,6 +26,8 @@ for platform in "${platforms[@]}"; do
             	-archivePath "archives/$PROJECT_NAME-$platform" \
             	SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcpretty
 done
+
+echo "Generating final xcframework..."
 
 xcodebuild -create-xcframework \
          -framework "archives/$PROJECT_NAME-iOS.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME.framework" \
@@ -26,3 +39,6 @@ xcodebuild -create-xcframework \
          -framework "archives/$PROJECT_NAME-macOS.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME.framework" \
          -output "$FRAMEWORK_NAME.xcframework"
 
+echo "Done."
+
+tree "$FRAMEWORK_NAME.xcframework"
